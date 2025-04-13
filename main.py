dd85f828e0d68850e0645475c1d9b4d3457306f6
Rômulo Henri <romulohenri000@gmail.com>
2025-04-12 23:38:03 -0300

    improve: gui crosshair n quit conditions

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 player = FirstPersonController()
+player.cursor.visible = False
 Sky()
+camera.fov = 75
+camera.clip_plane_far = 1000
 
 blocks = []
 
@@ -20,10 +23,28 @@ for x in range(20):
         )
         blocks.append(block)
 
+crosshair = Entity(
+    parent=camera.ui,
+    model='quad',
+    color=color.gray,
+    scale=(0.003, 0.03),
+    position=(0, 0)
+)
+crosshair2 = Entity(
+    parent=camera.ui,
+    model='quad',
+    color=color.gray,
+    scale=(0.03, 0.003),
+    position=(0, 0)
+)
+
 # Put Blocks
 def input(key):
     for block in blocks:
         if block.hovered:
+            if key == 'escape':
+                application.quit()
+
             if key == 'left mouse down':
                 blocks.remove(block)
                 destroy(block)
@@ -38,5 +59,9 @@ def input(key):
                     origin_y=0.5
                 )
                 blocks.append(new_block)
+
+def update():
+    if player.y < -20:
+        application.quit()
 
 app.run()
